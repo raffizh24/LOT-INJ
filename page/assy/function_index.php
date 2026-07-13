72d3a43890a6a9db11eb84af9caeb2c84f2eb16a
Muhammad Raffi <raffikecil85@gmail.com>
2026-07-13 08:03:20 +0700

    Update

--- page/assy/function_index.php
+++ page/assy/function_index.php
@@ -2,7 +2,6 @@
 // DATABASE & SESSION
 require '../../conn.php';
 session_start();
-
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'Assy') {
     header('location: ../../index.php');
     exit;
@@ -183,21 +182,6 @@ if (isset($_POST['btn_finish'])) {
     mysqli_begin_transaction($conn);
 
     try {
-        // Cek stock injection cukup
-        $stockCheck = mysqli_query($conn, "
-            SELECT qty_injection
-            FROM part 
-            WHERE part_code = '$partCode'
-        ");
-
-        if (mysqli_num_rows($stockCheck) == 0) {
-            throw new Exception('Part not found');
-        }
-
-        $stockRow = mysqli_fetch_assoc($stockCheck);
-        if ($stockRow['qty_injection'] < $qty) {
-            throw new Exception('Insufficient stock in injection');
-        }
         // INSERT TRANSACTION ASSY
         if (!mysqli_query($conn, "
             INSERT INTO `transaction`
